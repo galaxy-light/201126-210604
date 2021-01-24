4c385ef0094fc3183c961ade875c9491d8fade0b
galaxy-light <74356952+galaxy-light@users.noreply.github.com>
2021-01-24 16:38:59 +0900

    Update Method_t06.java

--- 201230/JAVA/1230/src/Method_t06.java
+++ 201230/JAVA/1230/src/Method_t06.java
@@ -3,21 +3,21 @@ public class Method_t06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		arr();
-//		// System.out.println(arr());
-//		
-//		int lotto[] = arr();
-//		// System.out.println(lotto);
-//
-//	}
-//	
-//	public static int [] arr() {
-//		int arr[] = new int [6];
-//		for (int i = 0; i < 6; i++) {
-//			i = (int) ((Math.random()*45)+1);
-//			System.out.println(arr[i]);			
-//		}
-//		return arr;
+		arr();
+		// System.out.println(arr());
+		
+		int lotto[] = arr();
+		// System.out.println(lotto);
+
+	}
+	
+	public static int [] arr() {
+		int arr[] = new int [6];
+		for (int i = 0; i < 6; i++) {
+			i = (int) ((Math.random()*45)+1);			
+			System.out.println(arr[i]);			
+		}
+		return arr;
 	}
 
 }
