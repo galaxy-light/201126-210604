7262e85e9c7b9f8a159902420ecd587f75bcd571
galaxy-light <vili1265@naver.com>
2021-02-01 17:31:30 +0900

    210201

--- 210201/0201_Test_05/src/Number.java
+++ 210201/0201_Test_05/src/Number.java
@@ -6,6 +6,17 @@ public class Number {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		String input = s.nextLine();
+		int [] alpha = new int[26];
+		
+		for (int i = 0; i < input.length(); i++) {
+			int index = input.charAt(i) + (int)'a';
+			alpha[index]++;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (alpha[i]!=0) {
+				String sentense = "" + (char)(i+97)+"'"+"는 "+alpha[i]+"글자입니다.";
+			}
+		}
 	}
 
 }
