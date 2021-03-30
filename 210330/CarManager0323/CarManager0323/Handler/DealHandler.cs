802e6506027976179b975b2ce65cffbb3a9f12aa
galaxy-light <vili1265@naver.com>
2021-03-30 16:54:06 +0900

    210330

---
+++ 210330/CarManager0323/CarManager0323/Handler/DealHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarManager0323.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManager0330.Handler
+{
+    class DealHandler
+    {
+        List<Deal> dealList = new List<Deal>();
+
+        public DealHandler()
+        {
+            dealList.Add(new Deal("0000", null, null, null)); // 임의의 값을 지정
+        }
+
+        public List<Deal> GetDealList()
+        {
+            return dealList; // get와 동일
+        }
+
+        public void dealListClear()
+        {
+            dealList[0].Car = null;
+            dealList[0].Customer = null;
+            dealList[0].Seller = null;
+            dealList[0].De_dat = "0000";         
+        }
+    }
+}
---
+++ 210330/CarManager0323/CarManager0323/Model/Deal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManager0323.Model
+{
+    class Deal
+    {
+        string de_dat;
+        Customer customer;
+        Car car;
+        Seller seller;
+
+        public Deal(string de_dat, Customer customer, Car car, Seller seller)
+        {
+            this.de_dat = de_dat;
+            this.customer = customer;
+            this.car = car;
+            this.seller = seller;
+        }
+
+        public string De_dat { get => de_dat; set => de_dat = value; }
+        internal Customer Customer { get => customer; set => customer = value; }
+        internal Car Car { get => car; set => car = value; }
+        internal Seller Seller { get => seller; set => seller = value; }
+    }
+}
