46fe5655f4babb9215bdb7d05a9d45bb707744fe
galaxy-light <vili1265@naver.com>
2021-05-03 08:54:40 +0900

    포트폴리오

---
+++ 포트폴리오/C#/주차/주차 관리 프로그램/Managing_Car_Program/Managing_Car_Program/Ui/DB_view_Form.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace Managing_Car_Program.Ui
+{
+    partial class DB_view_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_okay = new System.Windows.Forms.Button();
+            this.uiTextBox_name = new Sunny.UI.UITextBox();
+            this.uiTextBox_carnum = new Sunny.UI.UITextBox();
+            this.uiTextBox_phone = new Sunny.UI.UITextBox();
+            this.uiTextBox_start = new Sunny.UI.UITextBox();
+            this.uiTextBox_end = new Sunny.UI.UITextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.uiRadioButton2 = new Sunny.UI.UIRadioButton();
+            this.uiRadioButton1 = new Sunny.UI.UIRadioButton();
+            this.uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
+            this.uiSymbolLabel2 = new Sunny.UI.UISymbolLabel();
+            this.uiSymbolLabel3 = new Sunny.UI.UISymbolLabel();
+            this.uiSymbolLabel4 = new Sunny.UI.UISymbolLabel();
+            this.uiSymbolLabel5 = new Sunny.UI.UISymbolLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 389);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_okay
+            // 
+            this.button_okay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(55)))), ((int)(((byte)(65)))));
+            this.button_okay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_okay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_okay.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_okay.ForeColor = System.Drawing.Color.White;
+            this.button_okay.Location = new System.Drawing.Point(490, 516);
+            this.button_okay.Margin = new System.Windows.Forms.Padding(0, 8, 0, 8);
+            this.button_okay.Name = "button_okay";
+            this.button_okay.Size = new System.Drawing.Size(105, 33);
+            this.button_okay.TabIndex = 17;
+            this.button_okay.Text = "닫기";
+            this.button_okay.UseVisualStyleBackColor = false;
+            // 
+            // uiTextBox_name
+            // 
+            this.uiTextBox_name.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox_name.FillColor = System.Drawing.Color.White;
+            this.uiTextBox_name.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.uiTextBox_name.Location = new System.Drawing.Point(874, 160);
+            this.uiTextBox_name.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox_name.Maximum = 2147483647D;
+            this.uiTextBox_name.Minimum = -2147483648D;
+            this.uiTextBox_name.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox_name.Name = "uiTextBox_name";
+            this.uiTextBox_name.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox_name.Size = new System.Drawing.Size(173, 29);
+            this.uiTextBox_name.TabIndex = 23;
+            // 
+            // uiTextBox_carnum
+            // 
+            this.uiTextBox_carnum.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox_carnum.FillColor = System.Drawing.Color.White;
+            this.uiTextBox_carnum.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.uiTextBox_carnum.Location = new System.Drawing.Point(874, 216);
+            this.uiTextBox_carnum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox_carnum.Maximum = 2147483647D;
+            this.uiTextBox_carnum.Minimum = -2147483648D;
+            this.uiTextBox_carnum.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox_carnum.Name = "uiTextBox_carnum";
+            this.uiTextBox_carnum.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox_carnum.Size = new System.Drawing.Size(173, 29);
+            this.uiTextBox_carnum.TabIndex = 24;
+            // 
+            // uiTextBox_phone
+            // 
+            this.uiTextBox_phone.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox_phone.FillColor = System.Drawing.Color.White;
+            this.uiTextBox_phone.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.uiTextBox_phone.Location = new System.Drawing.Point(874, 273);
+            this.uiTextBox_phone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox_phone.Maximum = 2147483647D;
+            this.uiTextBox_phone.Minimum = -2147483648D;
+            this.uiTextBox_phone.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox_phone.Name = "uiTextBox_phone";
+            this.uiTextBox_phone.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox_phone.Size = new System.Drawing.Size(173, 29);
+            this.uiTextBox_phone.TabIndex = 25;
+            // 
+            // uiTextBox_start
+            // 
+            this.uiTextBox_start.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox_start.FillColor = System.Drawing.Color.White;
+            this.uiTextBox_start.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.uiTextBox_start.Location = new System.Drawing.Point(874, 329);
+            this.uiTextBox_start.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox_start.Maximum = 2147483647D;
+            this.uiTextBox_start.Minimum = -2147483648D;
+            this.uiTextBox_start.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox_start.Name = "uiTextBox_start";
+            this.uiTextBox_start.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox_start.Size = new System.Drawing.Size(173, 29);
+            this.uiTextBox_start.TabIndex = 26;
+            // 
+            // uiTextBox_end
+            // 
+            this.uiTextBox_end.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox_end.FillColor = System.Drawing.Color.White;
+            this.uiTextBox_end.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.uiTextBox_end.Location = new System.Drawing.Point(874, 386);
+            this.uiTextBox_end.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox_end.Maximum = 2147483647D;
+            this.uiTextBox_end.Minimum = -2147483648D;
+            this.uiTextBox_end.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox_end.Name = "uiTextBox_end";
+            this.uiTextBox_end.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox_end.Size = new System.Drawing.Size(173, 29);
+            this.uiTextBox_end.TabIndex = 27;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(501, 131);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 28;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.uiRadioButton2);
+            this.groupBox1.Controls.Add(this.uiRadioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.groupBox1.Location = new System.Drawing.Point(501, 317);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 118);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "정기권 날짜 지정";
+            // 
+            // uiRadioButton2
+            // 
+            this.uiRadioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiRadioButton2.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiRadioButton2.Location = new System.Drawing.Point(31, 68);
+            this.uiRadioButton2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiRadioButton2.Name = "uiRadioButton2";
+            this.uiRadioButton2.Padding = new System.Windows.Forms.Padding(22, 0, 0, 0);
+            this.uiRadioButton2.RadioButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(55)))), ((int)(((byte)(65)))));
+            this.uiRadioButton2.Size = new System.Drawing.Size(159, 29);
+            this.uiRadioButton2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiRadioButton2.TabIndex = 1;
+            this.uiRadioButton2.Text = "정기권 종료일 지정";
+            // 
+            // uiRadioButton1
+            // 
+            this.uiRadioButton1.Checked = true;
+            this.uiRadioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiRadioButton1.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiRadioButton1.Location = new System.Drawing.Point(31, 31);
+            this.uiRadioButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiRadioButton1.Name = "uiRadioButton1";
+            this.uiRadioButton1.Padding = new System.Windows.Forms.Padding(22, 0, 0, 0);
+            this.uiRadioButton1.RadioButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(55)))), ((int)(((byte)(65)))));
+            this.uiRadioButton1.Size = new System.Drawing.Size(159, 29);
+            this.uiRadioButton1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiRadioButton1.TabIndex = 0;
+            this.uiRadioButton1.Text = "정기권 시작일 지정";
+            // 
+            // uiSymbolLabel1
+            // 
+            this.uiSymbolLabel1.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiSymbolLabel1.Location = new System.Drawing.Point(789, 160);
+            this.uiSymbolLabel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolLabel1.Name = "uiSymbolLabel1";
+            this.uiSymbolLabel1.Padding = new System.Windows.Forms.Padding(34, 0, 0, 0);
+            this.uiSymbolLabel1.Size = new System.Drawing.Size(78, 30);
+            this.uiSymbolLabel1.Symbol = 62141;
+            this.uiSymbolLabel1.SymbolSize = 30;
+            this.uiSymbolLabel1.TabIndex = 30;
+            this.uiSymbolLabel1.Text = "이름";
+            // 
+            // uiSymbolLabel2
+            // 
+            this.uiSymbolLabel2.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiSymbolLabel2.Location = new System.Drawing.Point(774, 216);
+            this.uiSymbolLabel2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolLabel2.Name = "uiSymbolLabel2";
+            this.uiSymbolLabel2.Padding = new System.Windows.Forms.Padding(34, 0, 0, 0);
+            this.uiSymbolLabel2.Size = new System.Drawing.Size(93, 31);
+            this.uiSymbolLabel2.Symbol = 61881;
+            this.uiSymbolLabel2.SymbolSize = 30;
+            this.uiSymbolLabel2.TabIndex = 31;
+            this.uiSymbolLabel2.Text = "차량번호";
+            // 
+            // uiSymbolLabel3
+            // 
+            this.uiSymbolLabel3.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiSymbolLabel3.Location = new System.Drawing.Point(774, 273);
+            this.uiSymbolLabel3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolLabel3.Name = "uiSymbolLabel3";
+            this.uiSymbolLabel3.Padding = new System.Windows.Forms.Padding(34, 0, 0, 0);
+            this.uiSymbolLabel3.Size = new System.Drawing.Size(93, 29);
+            this.uiSymbolLabel3.Symbol = 57488;
+            this.uiSymbolLabel3.SymbolSize = 30;
+            this.uiSymbolLabel3.TabIndex = 32;
+            this.uiSymbolLabel3.Text = "전화번호";
+            // 
+            // uiSymbolLabel4
+            // 
+            this.uiSymbolLabel4.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiSymbolLabel4.Location = new System.Drawing.Point(736, 329);
+            this.uiSymbolLabel4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolLabel4.Name = "uiSymbolLabel4";
+            this.uiSymbolLabel4.Padding = new System.Windows.Forms.Padding(34, 0, 0, 0);
+            this.uiSymbolLabel4.Size = new System.Drawing.Size(131, 34);
+            this.uiSymbolLabel4.Symbol = 62065;
+            this.uiSymbolLabel4.SymbolSize = 30;
+            this.uiSymbolLabel4.TabIndex = 33;
+            this.uiSymbolLabel4.Text = "정기권 시작일";
+            // 
+            // uiSymbolLabel5
+            // 
+            this.uiSymbolLabel5.Font = new System.Drawing.Font("굴림", 11.25F);
+            this.uiSymbolLabel5.Location = new System.Drawing.Point(736, 384);
+            this.uiSymbolLabel5.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolLabel5.Name = "uiSymbolLabel5";
+            this.uiSymbolLabel5.Padding = new System.Windows.Forms.Padding(34, 0, 0, 0);
+            this.uiSymbolLabel5.Size = new System.Drawing.Size(131, 33);
+            this.uiSymbolLabel5.Symbol = 62068;
+            this.uiSymbolLabel5.SymbolSize = 30;
+            this.uiSymbolLabel5.TabIndex = 34;
+            this.uiSymbolLabel5.Text = "정기권 종료일";
+            // 
+            // DB_view_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1085, 583);
+            this.Controls.Add(this.uiSymbolLabel5);
+            this.Controls.Add(this.uiSymbolLabel4);
+            this.Controls.Add(this.uiSymbolLabel3);
+            this.Controls.Add(this.uiSymbolLabel2);
+            this.Controls.Add(this.uiSymbolLabel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.uiTextBox_end);
+            this.Controls.Add(this.uiTextBox_start);
+            this.Controls.Add(this.uiTextBox_phone);
+            this.Controls.Add(this.uiTextBox_carnum);
+            this.Controls.Add(this.uiTextBox_name);
+            this.Controls.Add(this.button_okay);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DB_view_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DB확인";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_okay;
+        private Sunny.UI.UITextBox uiTextBox_name;
+        private Sunny.UI.UITextBox uiTextBox_carnum;
+        private Sunny.UI.UITextBox uiTextBox_phone;
+        private Sunny.UI.UITextBox uiTextBox_start;
+        private Sunny.UI.UITextBox uiTextBox_end;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel1;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel2;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel3;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel4;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel5;
+        private Sunny.UI.UIRadioButton uiRadioButton2;
+        private Sunny.UI.UIRadioButton uiRadioButton1;
+    }
+}
---
+++ 포트폴리오/C#/주차/주차 관리 프로그램/Managing_Car_Program/Managing_Car_Program/Ui/LogForm.cs
@@ -0,0 +1,82 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Managing_Car_Program
+{
+    partial class LogForm : MaterialForm
+    {
+        string adminid = "admin";
+        string adminpw = "wnck";
+
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            loginsec();
+        }
+
+        private void textBox_id_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Tab)
+            {
+                textBox_pw.Focus();
+            }
+        }
+
+        private void textBox_pw_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                button_login.Focus();
+            }
+        }
+
+        private void loginsec()
+        {
+            if (textBox_id.Text == adminid)
+            {
+                if (textBox_pw.Text == adminpw)
+                {
+                    new AdminForm().ShowDialog();
+                    textBox_id.Text = "";
+                    textBox_pw.Text = "";
+
+                    string str = $"관리자로 로그인했습니다.";
+                    loginwriteLog(str);
+                    DataManager.Save();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("비밀번호가 일치하지 않습니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("일치하는 회원정보가 없습니다.");
+            }
+        }
+
+        private void uiSymbolButton_help3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("관리자 ID/PW 문의 : 000-0000-0000");
+        }
+
+        private void loginwriteLog(string txtcontents)
+        {
+            string logcontents = $"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}]{txtcontents}";
+            DataManager.printLog(logcontents);
+        }
+    }
+}
