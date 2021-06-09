9c50eb8a87a1373df9605471372e43a3f35798de
galaxy-light <vili1265@naver.com>
2021-06-09 22:29:36 +0900

    개인 공부

---
+++ 개인 공부/다이아몬드/ConsoleApp_Ex/ConsoleApp_Ex/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 10; j >= i; j--)
+                {
+                    //Console.Write("-");
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 10; j > i; j--)
+                {
+                    //Console.Write("-");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    //Console.Write("-");
+                    Console.Write(" ");
+                }
+                for (int j = 10; j >= i; j--)
+                {
+
+                    Console.Write("*");
+                }
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    //Console.Write("-");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
